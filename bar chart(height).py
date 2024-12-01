26643a2d785e0881976778ec5a0ee3103031ac37
BlaxkJesus <danieljames13182@gmail.com>
2024-12-01 23:51:11 +0100

    bar chart assignment.py

--- bar chart(height).py
+++ bar chart(height).py
@@ -1,21 +1,7 @@
 import matplotlib.pyplot as plt
-import numpy as np
 
-width = 0.4
+food = ['spinach','sausage','prawns','pineapple','mushroom']
+y = [40,20,10,44,23]
 
-x = [1,2,3,4,5,6,7,8,9,10]
-female = [175,158,166,175,160,165,166,170,170,172]
-male = [184,182,180,191,189,181,180,170,176,185]
-
-bar1= np.arange(len(x))
-bar2 = [i+width for i in bar1]
-
-plt.bar(bar1,male,width,label = 'Males')
-plt.bar(bar2,female,width,label = 'Females')
-
-plt.xlabel('Sequence')
-plt.ylabel('height')
-plt.title('Height between male and females')
-plt.xticks()
-plt.legend()
+plt.barh(food,y,0.5, color='purple')
 plt.show()
